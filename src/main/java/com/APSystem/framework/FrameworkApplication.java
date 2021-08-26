bddd62bb9a58570e1e7b2f4df1bca8885ae270b6
saber pourmomen <saber.pourmomen@gmail.com>
2021-08-26 18:23:33 +0430

    first initial

--- src/main/java/com/APSystem/framework/FrameworkApplication.java
+++ src/main/java/com/APSystem/framework/FrameworkApplication.java
@@ -1,4 +1,4 @@
-package com.APSystem.framework;
+package com.apsystem.framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FrameworkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FrameworkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkApplication.class, args);
+    }
 
 }
--- src/test/java/com/APSystem/framework/FrameworkApplicationTests.java
+++ src/test/java/com/APSystem/framework/FrameworkApplicationTests.java
@@ -1,4 +1,4 @@
-package com.APSystem.framework;
+package com.apsystem.framework;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FrameworkApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
